31f82b2937d3096fb083134b12c8663f34a9b734
Nikita Gavrilov <pelmeshka1110@gmail.com>
2026-04-28 01:51:19 +0200

    fix NRE

--- Code/UI/Editing/Controls/EnumControl.cs
+++ Code/UI/Editing/Controls/EnumControl.cs
@@ -171,7 +171,7 @@ public class EnumControl : BaseControl
 
 
 
-			return _flags ? _value.HasFlag( (Enum)value ) : _value.Equals( value );
+			return _flags ? (_value?.HasFlag( (Enum)value ) ?? false) : (_value?.Equals( value ) ?? false);
 		}
 
 		private void Select( Option option )
