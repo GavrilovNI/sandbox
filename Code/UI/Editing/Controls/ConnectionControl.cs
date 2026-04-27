ec4377e96d545ccbe16039a942ea996649e89acd
Nikita Gavrilov <pelmeshka1110@gmail.com>
2026-04-28 01:51:46 +0200

    add connection control, add owner to hierarchy

---
+++ Code/UI/Editing/Controls/ConnectionControl.cs
@@ -0,0 +1,64 @@
+﻿namespace Sandbox.UI.Editing.Controls;
+
+[CustomEditor( typeof( Connection ) )]
+public class ConnectionControl : BaseControl
+{
+	public override bool SupportsMultiEdit => true;
+
+	private DropDown? _dropDown;
+
+	public ConnectionControl()
+	{
+
+	}
+
+
+	public override void Rebuild()
+	{
+		if ( Property == null )
+		{
+			DeleteChildren();
+			return;
+		}
+
+		CreateDropdown();
+	}
+
+	private void CreateDropdown()
+	{
+		_dropDown = AddChild( new DropDown() );
+
+		_dropDown.Options.Add( new Option( "None", string.Empty, null ) );
+		foreach ( var connection in Connection.All )
+		{
+			_dropDown.Options.Add( new Option( connection.DisplayName, string.Empty, connection ) );
+		}
+
+		_dropDown.Value = Property.GetValue<object>();
+		_dropDown.ValueChanged = SetValue;
+		UpdateText();
+	}
+
+	private void SetValue( string value )
+	{
+		Connection connection;
+		if ( value is null || value == string.Empty )
+		{
+			connection = null;
+		}
+		else
+		{
+			connection = Connection.All.FirstOrDefault( x => x.ToString() == value );
+			if ( connection is null )
+				return;
+		}
+
+		Property.SetValue( connection );
+		UpdateText();
+	}
+
+	private void UpdateText()
+	{
+		_dropDown?.Text = Property.GetValue<Connection>()?.DisplayName ?? "None";
+	}
+}
